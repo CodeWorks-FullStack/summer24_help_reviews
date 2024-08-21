c67b9c3a8ac049558c3c73423c3aabe64f174fea
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-21 12:24:07 -0600

    models

---
+++ client/src/models/Restaurant.js
@@ -0,0 +1,16 @@
+import { DatabaseItem } from "./DatabaseItem.js";
+import { Profile } from "./Profile.js";
+
+export class Restaurant extends DatabaseItem {
+  constructor(data) {
+    super(data)
+    this.name = data.name
+    this.imgUrl = data.imgUrl
+    this.description = data.description
+    this.visits = data.visits
+    this.isShutdown = data.isShutdown
+    this.creatorId = data.creatorId
+    this.creator = new Profile(data.creator)
+  }
+}
+
